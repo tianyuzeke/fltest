90b9463f1405fb3de805a8ab0a103d7e33058d16
lty <lty1856@gmail.com>
2022-08-11 22:14:56 +0800

    fedavg

---
+++ fedavg1.py
@@ -0,0 +1,181 @@
+import argparse
+from pickletools import optimize
+import torch
+from torch import nn
+from torch.optim import Adam, SGD
+from torch.utils.data import DataLoader, random_split
+import os, random
+from copy import deepcopy
+from pickle_dataset import PickleDataset
+from tqdm import trange
+
+from fedlab.utils.logger import Logger
+
+from utils import get_args, AverageMeter
+
+def get_optimizer(model, optimizer, args=None):
+    if args is None:
+        args = {}
+    if optimizer == "sgd":
+        return SGD(model.parameters(), lr=args.get('lr', 2e-2))
+    elif optimizer == "momentum_sgd":
+        return SGD(model.parameters(), lr=args.get('lr', 1e-2), momentum=args.get('momentum', 0.9))
+    elif optimizer == "adam":
+        _betas = (0.9, 0.999) if "betas" not in args.keys() else args["betas"]
+        return Adam(model.parameters(), lr=args.get('lr', 1e-3), betas=_betas)
+    raise NotImplementedError
+
+
+class EmnistCNN(nn.Module):
+    def __init__(self):
+        super(EmnistCNN, self).__init__()
+        self.net = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, stride=2, padding=0),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, stride=2, padding=0),
+            nn.Flatten(),
+            nn.Linear(7 * 7 * 64, out_features=1024),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.6),
+            nn.Linear(in_features=1024, out_features=62),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+class LocalTrainer:
+    def __init__(self, optimizer_type, optimizer_args, criterion,
+            epochs, batch_size, pers_round, cuda) -> None:
+        self.dataset = PickleDataset("femnist", pickle_root='../leaf/pickle_datasets')
+        self.optimizer_type = optimizer_type
+        self.optimizer_args = optimizer_args
+        self.criterion = criterion
+        self.epochs = epochs
+        self.pers_round = pers_round
+        self.batch_size = batch_size
+        self.cuda = cuda
+    
+    def train(self, model, client_id):
+        trainloader = DataLoader(
+            self.dataset.get_dataset_pickle("train", client_id), self.batch_size
+        )
+
+        freezed_model = deepcopy(model)
+        optimizer = get_optimizer(model, self.optimizer_type, self.optimizer_args)
+        self._train(client_id, model, trainloader, optimizer, self.epochs)
+
+        gradients = []
+        for old_param, new_param in zip(freezed_model.parameters(), model.parameters()):
+            gradients.append(old_param.data - new_param.data)
+        
+        weight = torch.tensor(len(trainloader.sampler))
+        return weight, gradients
+
+    def _train(self, client_id, model, dataloader, optimizer, epochs):
+        model.train()
+        for _ in trange(epochs, desc="client [{}]".format(client_id)):
+            for x, y in dataloader:
+                if self.cuda != -1:
+                    x, y = x.cuda(self.cuda), y.cuda(self.cuda)
+
+                outputs = model(x)
+                loss = self.criterion(outputs, y)
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+    def evaluate(self, model, test_clients):
+        model.eval()
+        # gpu = next(model.parameters()).device
+
+        loss = AverageMeter()
+        acc = AverageMeter()
+
+        with torch.no_grad():
+            for client in test_clients:
+                testloader = DataLoader(
+                    self.dataset.get_dataset_pickle("test", client), self.batch_size
+                )
+                for inputs, labels in testloader:
+                    if self.cuda != -1:
+                        inputs = inputs.cuda(self.cuda)
+                        labels = labels.cuda(self.cuda)
+
+                    outputs = model(inputs)
+                    l = criterion(outputs, labels)
+
+                    _, predicted = torch.max(outputs, 1)
+                    loss.update(l.item(), len(labels))
+                    acc.update(torch.sum(predicted.eq(labels)).item(), len(labels))
+
+        return loss.average(), acc.average()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    args = get_args(parser)
+
+    # logger = Logger(log_name="Personalized FedAvg")
+    if args.cuda == -1:
+        device = torch.device("cpu")
+    else:
+        device = torch.device("cuda:%d" % args.cuda)
+
+    global_model = EmnistCNN().to(device)
+    global_optimizer = get_optimizer(
+        global_model, "sgd", dict(lr=args.server_lr, momentum=0.9)
+    )
+    criterion = torch.nn.CrossEntropyLoss()
+
+    # seperate clients
+    num_training_clients = int(0.8 * args.client_num_in_total)
+    training_clients_id_list = range(num_training_clients)
+    num_testing_clients = args.client_num_in_total - num_training_clients
+    test_clients_id_list = range(num_testing_clients)
+
+    trainer = LocalTrainer(
+        optimizer_type="sgd",
+        optimizer_args=dict(lr=args.local_lr),
+        criterion=criterion,
+        epochs=args.inner_loops,
+        batch_size=args.batch_size,
+        pers_round=args.pers_round,
+        cuda=args.cuda
+    )
+    
+    random.seed(0)
+    test_clients = random.sample(test_clients_id_list, 40)
+
+    # FedAvg training
+    for e in range(args.epochs):
+        selected_clients = random.sample(
+            training_clients_id_list, args.client_num_per_round
+        )
+        all_client_weights = []
+        all_client_gradients = []
+
+        for client_id in selected_clients:
+            weight, grads = trainer.train(deepcopy(global_model), client_id)
+            all_client_weights.append(weight)
+            all_client_gradients.append(grads)
+
+        # FedAvg aggregation (momentum SGD)
+        global_optimizer.zero_grad()
+        weights_sum = sum(all_client_weights)
+        all_client_weights = [weight / weights_sum for weight in all_client_weights]
+        for weight, grads in zip(all_client_weights, all_client_gradients):
+            for param, grad in zip(global_model.parameters(), grads):
+                if param.grad is None:
+                    param.grad = torch.zeros(
+                        param.size(), requires_grad=True, device=param.device
+                    )
+                param.grad.data.add_(grad.data * weight)
+        global_optimizer.step()
+
+        if e % 20 == 0:
+            loss, acc = trainer.evaluate(deepcopy(global_model), test_clients)
+            print(loss, acc)
---
+++ utils.py
@@ -0,0 +1,105 @@
+
+class AverageMeter:
+    """Record metrics information"""
+
+    def __init__(self):
+        self.sum = self.count = 0.0
+
+    def reset(self):
+        self.sum = self.count = 0.0
+
+    def update(self, val, n=1):
+        self.sum += val
+        self.count += n
+    
+    def average(self):
+        return self.sum / self.count
+
+def get_args(parser):
+    parser.add_argument(
+        "--client_num_in_total", type=int, default=3597, help="total num of clients",
+    )
+    parser.add_argument(
+        "--client_num_per_round",
+        type=int,
+        default=20,
+        help="determine how many clients join training in one communication round",
+    )
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=20,
+        help="batch size of local training in FedAvg and fine-tune",
+    )
+    parser.add_argument("--epochs", type=int, default=500, help="communication round")
+    parser.add_argument(
+        "--inner_loops", type=int, default=10, help="local epochs in FedAvg section"
+    )
+    parser.add_argument(
+        "--server_lr",
+        type=float,
+        default=1.0,
+        help="server optimizer lr in FedAvg section",
+    )
+    parser.add_argument(
+        "--local_lr",
+        type=float,
+        default=2e-2,
+        help="local optimizer lr in FedAvg section",
+    )
+    parser.add_argument(
+        "--fine_tune",
+        type=bool,
+        default=True,
+        help="determine whether perform fine-tune",
+    )
+    parser.add_argument(
+        "--fine_tune_outer_loops",
+        type=int,
+        default=100,
+        help="outer epochs in fine-tune section",
+    )
+    parser.add_argument(
+        "--fine_tune_inner_loops",
+        type=int,
+        default=10,
+        help="inner epochs in fine-tune section",
+    )
+    parser.add_argument(
+        "--fine_tune_server_lr",
+        type=float,
+        default=1e-2,
+        help="server optimizer lr in fine-tune section",
+    )
+    parser.add_argument(
+        "--fine_tune_local_lr",
+        type=float,
+        default=1e-3,
+        help="local optimizer lr in fine-tune section",
+    )
+    parser.add_argument(
+        "--test_round", type=int, default=100, help="num of round of final testing"
+    )
+    parser.add_argument(
+        "--pers_round", type=int, default=5, help="num of round of personalization"
+    )
+    parser.add_argument(
+        "--cuda",
+        type=int,
+        default=-1,
+        # action='store_false',
+        help="GPU index, -1 for using CPU",
+    )
+    parser.add_argument(
+        "--struct",
+        type=str,
+        default="cnn",
+        help="architecture of model, expected of mlp or cnn",
+    )
+    parser.add_argument("--ip", type=str, default="127.0.0.1")
+    parser.add_argument("--port", type=str, default="3002")
+    parser.add_argument("--rank", type=int, default=0)
+    parser.add_argument("--world_size", type=int, default=4)
+    parser.add_argument("--ethernet", type=str, default=None)
+    _args = parser.parse_args()
+    return _args
