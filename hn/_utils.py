1635c70bd5c7c1113286f8e80360055b7a7aeb5f
lty <lty1856@gmail.com>
2022-10-23 22:03:38 +0800

    hn_tpls

---
+++ hn/_utils.py
@@ -0,0 +1,38 @@
+import sys
+
+sys.path.append('..')
+from utils import str2bool
+
+def get_args(parser):
+    parser.add_argument("--suffix", type=str, default="", help="suffix")
+
+    parser.add_argument(
+        "--data-name", type=str, default="cifar10", choices=['cifar10', 'cifar100'], help="dir path for MNIST dataset"
+    )
+    parser.add_argument("--data-path", type=str, default="data", help="dir path for MNIST dataset")
+    parser.add_argument("--num-nodes", type=int, default=50, help="number of simulated nodes")
+
+    parser.add_argument("--clients-per-round", type=int, default=1)
+    parser.add_argument("--num-steps", type=int, default=5000)
+    parser.add_argument("--optim", type=str, default='sgd', choices=['adam', 'sgd'], help="learning rate")
+    parser.add_argument("--batch-size", type=int, default=64)
+    parser.add_argument("--inner-steps", type=int, default=50, help="number of inner steps")
+
+    parser.add_argument("--n-hidden", type=int, default=3, help="num. hidden layers")
+    parser.add_argument("--inner-lr", type=float, default=5e-3, help="learning rate for inner optimizer")
+    parser.add_argument("--lr", type=float, default=1e-2, help="learning rate")
+    parser.add_argument("--wd", type=float, default=1e-3, help="weight decay")
+    parser.add_argument("--inner-wd", type=float, default=5e-5, help="inner weight decay")
+    parser.add_argument("--embed-dim", type=int, default=-1, help="embedding dim")
+    parser.add_argument("--embed-lr", type=float, default=None, help="embedding learning rate")
+    parser.add_argument("--hyper-hid", type=int, default=100, help="hypernet hidden dim")
+    parser.add_argument("--spec-norm", type=str2bool, default=False, help="hypernet hidden dim")
+    parser.add_argument("--n-kernels", type=int, default=16, help="number of kernels for cnn model")
+
+    parser.add_argument("--cuda", type=int, default=-1, help="gpu device ID")
+    parser.add_argument("--eval-every", type=int, default=30, help="eval every X selected epochs")
+    parser.add_argument("--save-path", type=str, default="pfedhn_hetro_res", help="dir path for output file")
+    parser.add_argument("--seed", type=int, default=42, help="seed value")
+
+    args = parser.parse_args()
+    return args
